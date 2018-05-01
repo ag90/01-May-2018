0ad45c61579ab8dbe34e6e744312820af0ac8dde
Avi Gupta <guptaav@HCL.COM>
2018-05-01 14:16:15 +0530

    data files

---
+++ src/main/java/OOP/Datatype.java
@@ -0,0 +1,47 @@
+package OOP;
+
+public class Datatype {
+
+	
+	public static void main(String[] args) {
+
+
+		char i=65;
+		System.out.println(i);
+		
+		char w=90;
+		System.out.println(w);
+		
+		char p=97;
+		System.out.println(p);
+		
+		char n=122;
+		System.out.println(n);
+		
+		char j='@';
+		System.out.println(j);
+		
+		int k=90;
+		System.out.println(k);
+		
+		long y=1111111111;
+		System.out.println(y);
+		
+		boolean d=true;
+		System.out.println(d);
+		
+		boolean q=true;
+		System.out.println(q);
+		
+		double x=111.10228;
+		System.out.println(x);
+		
+		byte h=122;
+		System.out.println(h);
+		
+	
+				
+		
+	}
+
+}
---
+++ src/test/java/demo/demo.java
@@ -0,0 +1,23 @@
+package demo;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	
+
+	@Test
+	public void m()
+	{
+		assertEquals("Hello Google Guys", "Hello Google Guys");
+	}
+	
+	@Test
+	public void m1()
+	{
+		System.out.println("WELCOME TO Google Campus");
+	}
+
+}
